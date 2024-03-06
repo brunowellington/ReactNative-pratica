d386cac1b8d38e7511e50842d140d0adac09aab8
brunowell-aLenda <brunowellufersa19@gmail.com>
2024-03-06 14:02:42 -0300

    Criando arquivos do banco para passar ao supabase

--- src/app/Recipes/index.tsx
+++ src/app/Recipes/index.tsx
@@ -1,7 +1,9 @@
 import { MaterialIcons } from "@expo/vector-icons";
-import { View } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import { styles } from "./styles";
 import { router } from "expo-router";
+import { Recipe } from "@/components/Recipe";
+import { Ingredients } from "@/components/Ingredients";
 
 // Funçao para voltar a tela interior: onPress={() => router.back()}
 
@@ -14,7 +16,24 @@ export default function Recipes() {
           size={32}
           onPress={() => router.back()}
         />
+        <Text style={styles.title}>Ingredientes</Text>
       </View>
+      {/* FlatList é para renderizar listas e usa o renderItem */}
+
+      <FlatList
+        data={["1"]}
+        keyExtractor={(item) => item}
+        renderItem={() => (
+          <Recipe
+            recipe={{
+              name: "Omelete",
+              image:
+                "https://cptstatic.s3.amazonaws.com/imagens/blogs/portal-agropecuario/materias/2012/10/producao-ovo-portal-agropecuario.jpg",
+              minutes: 10,
+            }}
+          />
+        )}
+      />
     </View>
   );
 }
